94522b88232f44ae143c870f16823b7023bdb627
SST1998 <songphon8425@gmail.com>
2023-10-13 18:32:46 +0700

    commit

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -126,7 +126,7 @@ const ProductDetail = () => {
                                 confirmButtonColor: '#000',
                                 confirmButtonText: 'OK'
                               }).then(() => {
-                                window.location.href = `/nike-clone/product-detail/${product.id}`
+                                window.location.hostname = `/nike-clone/product-detail/${product.id}`
                               })
                             } else {
                               Swal.fire({
